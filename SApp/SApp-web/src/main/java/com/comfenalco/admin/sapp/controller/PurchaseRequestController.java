8d6881a66acb27a0b2f05fb1e2b3a89b1c2590ea
rocker <ing.rockerdiaz@gmail.com>
2013-12-27 17:57:06 -0500

    Trabajo sobre el modulo dell jefe de area. Creacion de una solicitud de compra, modificacion de datos de la cuenta

--- SApp/SApp-web/src/main/java/com/comfenalco/admin/sapp/controller/PurchaseRequestController.java
+++ SApp/SApp-web/src/main/java/com/comfenalco/admin/sapp/controller/PurchaseRequestController.java
@@ -3,31 +3,82 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.comfenalco.admin.sapp.controller;
 
+import java.io.Serializable;
 import java.sql.Date;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
 
 /**
  *
  * @author rockerW7
  */
 @Named(value = "purchaseRequestController")
-@RequestScoped
-public class PurchaseRequestController {
-    
+@SessionScoped
+public class PurchaseRequestController implements Serializable {
+
     private Integer idPurchaseRequest;
     private Date sendDate;
     private String observations;
+    private short state;
     
-    
+    @Inject private OrderController orderController;
+
+    @Inject private ProductController productController;
 
     /**
      * Creates a new instance of PurchaseRequestController
      */
     public PurchaseRequestController() {
     }
-    
+
+    /**
+     * @Summary: Metodo que permite crear en memoria los objetos involucrados en
+     * la solicitud de compra (formulario 'create.xhtml')
+     */
+    public void onAdd() {
+        System.out.println("Clicked in PurchaseRequestController!");
+        
+        orderController.add();
+        /*productController.add();
+
+        System.out.println("OrderList:");
+        for( Order1 order : orderController.getOrderList() ){
+            System.out.println("Order Object: " + order);
+        }*/
+    }
+
+    public Integer getIdPurchaseRequest() {
+        return idPurchaseRequest;
+    }
+
+    public void setIdPurchaseRequest(Integer idPurchaseRequest) {
+        this.idPurchaseRequest = idPurchaseRequest;
+    }
+
+    public Date getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(Date sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public short getState() {
+        return state;
+    }
+
+    public void setState(short state) {
+        this.state = state;
+    }
 }
